3dd4cbdb89019eae2ab26d5e7ef16f4898b714a3
Arturo Quiroz <r2inlondon@gmail.com>
2022-04-24 17:28:21 +0100

    feature: autoplay implemented

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#image-carousel", {
     heightRatio: 0.5,
-    type: "loop",
+    type: "fade",
+    rewind: true,
+    autoplay: true,
   }).mount();
 });
