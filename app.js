987a9fbfa30bb7c151b515bf86a8399b77e97264
Arturo Quiroz <r2inlondon@gmail.com>
2022-04-24 18:03:34 +0100

    feat: responsives created

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   new Splide("#image-carousel", {
-    heightRatio: 0.5,
+    // heightRatio is to crop the images
+    heightRatio: 0.4,
     type: "fade",
     rewind: true,
     autoplay: true,
+    breakpoints: {
+      640: {
+        heightRatio: 1.5,
+        cover: true,
+      },
+    },
   }).mount();
 });
